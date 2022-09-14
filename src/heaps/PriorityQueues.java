890db732ba3cd37ebd2a33dc0347b419cc840275
shubham pathak <95shubham1@gmail.com>
2022-09-14 21:04:35 +0530

    implementing heaps

--- src/heaps/PriorityQueues.java
+++ src/heaps/PriorityQueues.java
@@ -2,9 +2,22 @@ package heaps;
 
 public class PriorityQueues {
 
+    // hop and complete bt
 
     public PriorityQueues(){
-        
+
+    }
+    public void add(int val){
+
+    }
+    public int remove(){
+
+    }
+    public int peek(){
+
+    }
+    public int size(){
+
     }
 
 }
