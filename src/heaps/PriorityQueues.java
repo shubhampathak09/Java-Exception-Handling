c11f49a05c6f9fa0784c22453f12ce6fa3761480
shubham pathak <95shubham1@gmail.com>
2022-09-14 21:28:34 +0530

    heaps

--- src/heaps/PriorityQueues.java
+++ src/heaps/PriorityQueues.java
@@ -1,20 +1,33 @@
 package heaps;
 
+import java.util.ArrayList;
+
 public class PriorityQueues {
 
     // hop and complete bt
-
+    ArrayList<Integer> data;
     public PriorityQueues(){
-
+     data=new ArrayList<>();
     }
     public void add(int val){
+    data.add(val);
+    upHeapify(data.size()-1);
 
+    }
+    private void upHeapify(int i){
+        
     }
     public int remove(){
 
     }
     public int peek(){
-
+    if(this.size() == 0) {
+        System.out.println("underflow");
+        return -1;
+    }
+    else {
+        return data.get(0);
+    }
     }
     public int size(){
 
